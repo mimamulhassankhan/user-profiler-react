67bfec142b55d7a487c16a4bda4af655d69321a7
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-07 23:27:42 +0600

    Updates

---
+++ precache-manifest.4f32f78898c3ea86c84e9be41cc8891a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b3d8f2e8d61851c72552c31ff3211d",
+    "url": "/user-profiler-react/index.html"
+  },
+  {
+    "revision": "e9c04ec71927d6ebb5b4",
+    "url": "/user-profiler-react/static/css/main.928741f6.chunk.css"
+  },
+  {
+    "revision": "2d48ea37427148f8447d",
+    "url": "/user-profiler-react/static/js/2.1bcc636e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-profiler-react/static/js/2.1bcc636e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c04ec71927d6ebb5b4",
+    "url": "/user-profiler-react/static/js/main.e42064ba.chunk.js"
+  },
+  {
+    "revision": "c23a841e3fce18c1dc7e",
+    "url": "/user-profiler-react/static/js/runtime-main.a2195c04.js"
+  }
+]);
